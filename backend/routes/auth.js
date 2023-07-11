114d1602d3b4cec5d17b97d6683bb091cd1d2189
saswat <saswatranjan0602@gmail.com>
2023-07-11 16:56:33 +0530

    login

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,62 +1,101 @@
 const express = require("express");
 const User = require("../models/User");
 const router = express.Router();
-const {body ,validationResult} = require ("express-validator") 
-const bcrypt = require('bcryptjs')
+const { body, validationResult } = require("express-validator");
+const bcrypt = require("bcryptjs");
 var jwt = require("jsonwebtoken");
 
 const JWT_SECRET = "Iama$oy";
 
-
 // Create a User using: POST "/api/auth/createuser". No login required
-router.post('/createuser', [
-  body('name', 'Enter a valid name').isLength({ min: 3 }),
-  body('email', 'Enter a valid email').isEmail(),
-  body('password', 'Password must be atleast 6 characters').isLength({ min: 6}),
+router.post(
+  "/createuser",
+  [
+    body("name", "Enter a valid name").isLength({ min: 3 }),
+    body("email", "Enter a valid email").isEmail(),
+    body("password", "Password must be atleast 6 characters").isLength({
+      min: 6,
+    }),
+  ],
+  async (req, res) => {
+    // If there are errors, return Bad request and the errors
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    // Check whether the user with this email exists already
+    try {
+      let user = await User.findOne({ email: req.body.email });
+      if (user) {
+        return res
+          .status(400)
+          .json({ error: "Sorry a user with this email already exists" });
+      }
+
+      const salt = await bcrypt.genSalt(10);
+      const secPass = await bcrypt.hash(req.body.password, salt);
+
+      // Create a new user
+      user = await User.create({
+        name: req.body.name,
+        password: secPass,
+        email: req.body.email,
+      });
+
+      const data = {
+        user: {
+          id: user.id,
+        },
+      };
+      const authToken = jwt.sign(data, JWT_SECRET);
+      res.json(authToken);
+    } catch (error) {
+      console.error(error.message);
+      res.status(500).send("Some Error occured");
+    }
+  }
+);
+
+
+
+
+// Authenticate a User using: POST "/api/auth/login". No login required
+router.post('/login', [ 
+  body('email', 'Enter a valid email').isEmail(), 
+  body('password', 'Password cannot be blank').exists(), 
 ], async (req, res) => {
+
   // If there are errors, return Bad request and the errors
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
     return res.status(400).json({ errors: errors.array() });
   }
-  // Check whether the user with this email exists already
+
+  const {email, password} = req.body;
   try {
-    let user = await User.findOne({ email: req.body.email });
-    if (user) {
-      return res.status(400).json({ error: "Sorry a user with this email already exists" })
+    let user = await User.findOne({email});
+    if(!user){
+      return res.status(400).json({error: "Please try to login with correct credentials"});
     }
 
+    const passwordCompare = await bcrypt.compare(password, user.password);
+    if(!passwordCompare){
+      return res.status(400).json({error: "Please try to login with correct credentials"});
+    }
 
-       const salt = await bcrypt.genSalt(10);
-       const secPass = await bcrypt.hash(req.body.password, salt);
-
- 
-    
-
-
-
-
-    // Create a new user
-    user = await User.create({
-      name: req.body.name,
-      password: secPass,
-      email: req.body.email,
-    });
-
-    const data={
+    const data = {
       user:{
-        id:user.id
+        id: user.id
+      }
     }
-    }
-    const authToken= jwt.sign(data,JWT_SECRET)
-    res.json(authToken);
-    
+    const authtoken = jwt.sign(data, JWT_SECRET);
+    res.json({authtoken})
+
   } catch (error) {
     console.error(error.message);
-    res.status(500).send("Some Error occured");
+    res.status(500).send("Internal Server Error");
   }
+
+
 })
-
-
-
-module.exports = router;
+module.exports = router
